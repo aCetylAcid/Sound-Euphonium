1e83f2b6417ead4bccf057f1124f457212408ee9
aCetylAcid <aCetylAcid@gmail.com>
2019-10-10 22:01:32 +0900

    MOD HLS番組対応に伴うリファクタリング

--- downloader.py
+++ downloader.py
@@ -12,45 +12,79 @@ import dateutil.parser
 from TwitterAPI import TwitterAPI
 import eyed3
 import ffmpeg
+from enum import Enum
 
-class Channel:
-    def __init__(self, channel_id):
-        channel_ids = channel_id.split(":")
-        self.id = channel_ids[0]  # Channel Id(Ex: euphonium)
+# 通知用
+twitter = None
 
-        if len(channel_ids) > 1:
-            self.num_id = channel_id.split(u":")[1]  # Channel number Id(Ex: 540)
-        else:
-            self.num_id = u""  # Channel number Id(Ex: 540)
+class AbstractEpisode:
+    def __init__(self, count_str, thumbnail_url, updated_date_str):
+        self.count_str = count_str
+        self.thumbnail_url = thumbnail_url
+        self.updated_date_str = updated_date_str
 
-        self.count = u""            # Count
-        self.sound_url = u""        # Sound URL
-        self.m3u8_file_url= u""     # m3u8 file URL
-        self.title = u""            # Title of Channel
-        self.file_name = u""        # Original file Name
-        self.updated_at = u""       # Update Date(String)
-        self.thumb_url = u""        # thumbnail path(image)
-        self.thumb_file_name = u""  # thumbnail file name
+    def source_file_name(self):
+        raise NotImplementedError()
 
-    def file_name_without_extension(self):
-        arr = self.file_name.split(".")
+    def source_file_name_without_extension(self):
+        arr = self.source_file_name().split(".")
         arr.pop()
         return ".".join(arr)
 
-    def needs_to_use_new_api(self):
-        return len(self.num_id) != 0
+    def thumb_file_name(self): 
+        return self.thumbnail_url.split("/")[-1]
+
+class HlsEpisode(AbstractEpisode):
+    def __init__(self, count_str, thumbnail_url, updated_date_str, playlist_file_url):
+        super().__init__(count_str, thumbnail_url, updated_date_str)
+        self.playlist_file_url = playlist_file_url
+
+    def source_file_name(self):
+        return self.playlist_file_url.split("/")[-2]
+
+class RawEpisode(AbstractEpisode):
+    def __init__(self, count_str, thumbnail_url, updated_date_str, source_file_url):
+        super().__init__(count_str, thumbnail_url, updated_date_str)
+        self.source_file_url = source_file_url
+
+    def source_file_name(self):
+        return self.source_file_url.split("/")[-1]
+
+    def is_mp4(self):
+        arr = self.source_file_name().split(".")
+        extension = arr.pop()
+        return extension == "mp4"
+
+class Channel:
+    class BroadcastType(Enum):
+        RAW = 1
+        HLS = 2
+
+    def __init__(self, channel_code):
+        channel_ids = channel_code.split(":")
+        self.identifier_string = channel_ids[0]  # Channel Id(Ex: euphonium)
+
+        if len(channel_ids) > 1:
+            self.broadcast_type = Channel.BroadcastType.HLS
+            self.identifier_number = channel_ids[1]  # Channel number Id(Ex: 540)
+        else:
+            self.broadcast_type = Channel.BroadcastType.RAW
+
+        self.title = u""
+        self.episodes = [] 
 
     # Load channel information from API
     def load_channel_info(self):
         # if num_id was specified, use new api
 
-        if self.needs_to_use_new_api():
+        if self.broadcast_type == Channel.BroadcastType.HLS:
             bearer_token = UserSettings.get("bearer_token")
             if not bearer_token:
                 raise BusinessException(u"Bearer Token is not specified in user_settings.yml.")
 
             try:
-                url = u"https://app.onsen.ag/api/me/programs/" + self.num_id
+                url = Consts.BASE_URL_GET_PROGRAM_INFO\
+                     .replace("{program_id}", self.identifier_number)
                 headers = {'Host': 'app.onsen.ag',
                            'X-Device-Os': 'ios',
                            'Accept': '*/*',
@@ -63,9 +97,9 @@ class Channel:
                            'User-Agent': 'iOS/Onsen/2.6.1',
                            'X-App-Version': '25'
                     }
-                
                 req = urllib.request.Request(url, None, headers=headers)
                 response = urllib.request.urlopen(req)
+
             except urllib.error.HTTPError as e:
                 if e.code == 404:
                     message = "This channel may not be published."
@@ -75,97 +109,129 @@ class Channel:
             
             r_str = response.read()
             r_json = json.loads(r_str)
-            latest_episode = (r_json["episodes"])[0]
+            for r_ep in r_json["episodes"]:
+                count_str = r_ep["title"]
+                thumbnail_url = (r_json["program_image"])["video_url"]
+                updated_date_str = dateutil.parser.parse(r_ep["updated_on"]).strftime('%Y.%m.%d')
+                playlist_file_url = ((r_ep["episode_files"])[0])["media_url"]
 
-            self.count = latest_episode["title"]
-            self.m3u8_file_url = ((latest_episode["episode_files"])[0])["media_url"]
+                self.episodes.append(HlsEpisode(count_str,
+                                                thumbnail_url, 
+                                                updated_date_str, 
+                                                playlist_file_url))
             self.title = r_json["title"]
-            self.file_name = self.m3u8_file_url.split("/")[-2]
-            self.updated_at = dateutil.parser.parse(latest_episode["updated_on"]).strftime('%Y.%m.%d')
-            self.thumb_url = (r_json["program_image"])["video_url"]
-            self.thumb_file_name = self.thumb_url.split("/")[-1]
            
-        else:
+        elif self.broadcast_type == Channel.BroadcastType.RAW:
             try:
-                response = urllib.request.urlopen(Utils.url_get_channel_info(self.id))
+                response = urllib.request.urlopen(Utils.url_get_channel_info(self.identifier_string))
             except urllib.error.HTTPError as e:
                 if e.code == 404:
                     message = "This channel may not be published."
                     raise BusinessException(message)
+                else:
+                    raise BusinessException(u"Unexpected error occured.")
             r_str = response.read().decode('utf-8')[9:-3]
             r_json = json.loads(r_str)
 
-            self.count = r_json["count"]
-            self.sound_url = (r_json["moviePath"])["pc"]
+            count_str = r_json["count"]
+            thumbnail_url = Consts.BASE_URL + r_json["thumbnailPath"]
+            updated_date_str = r_json["update"]
+            source_file_url = (r_json["moviePath"])["pc"]
+
+            self.episodes.append(RawEpisode(count_str, thumbnail_url, updated_date_str, source_file_url))
+
             self.title = r_json["title"]
-            self.file_name = (r_json["moviePath"])["pc"].split("/")[-1]
-            self.updated_at = r_json["update"]
-            self.thumb_url = Consts.BASE_URL + r_json["thumbnailPath"]
-            self.thumb_file_name = r_json["thumbnailPath"].split("/")[-1]
+
+        else:
+            raise FatalException("")
 
 
 class Downloader:
     # Download Channel
     @staticmethod
-    def downloadChannel(channel):
-        
+    def download_channel(channel):
         dir_path = Utils.radio_save_dir_path(channel)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
     
-        file_path = Utils.radio_save_file_path(channel)
-        if os.path.exists(file_path):
-            raise BusinessException("Already Downloaded:"
-                                        + file_path)
-
-        mp3_file_path = u""
-
-        if channel.needs_to_use_new_api():
+        if channel.broadcast_type == Channel.BroadcastType.HLS:
             # download m3u8 plist file with ffmpeg
-            mp4_file_path = dir_path + channel.file_name 
-            mp3_file_path = dir_path + channel.file_name_without_extension() + ".mp3"
+            for episode in channel.episodes:
+                mp4_file_path = dir_path + episode.source_file_name()
+                mp3_file_path = dir_path + episode.source_file_name_without_extension() + ".mp3"
 
-            cmd = '''\
-                ffmpeg \
-                -protocol_whitelist file,http,https,tcp,tls,crypto \
-                -i "{m3u8_file_url}" \
-                -c copy {file_path}
-                -user_agent "AppleCoreMedia/1.0.0.16A366 (iPhone; U; CPU OS 12_0 like Mac OS X; ja_jp)" \
-                -headers "Accept: */*" \
-                -headers "Accept-Language: ja-jp" \
-                -headers "Accept-Encoding: gzip" \
-                -headers "Connection: keep-alive" \
-                -vn \
-                '''.format(m3u8_file_url = channel.m3u8_file_url, file_path = mp4_file_path).strip()
-            subprocess.call(cmd, shell=True)
+                if os.path.exists(mp4_file_path):
+                    logging.info("Skip " + channel.identifier_string + " " + episode.count_str + ", because already downloaded.")
+                    continue
+    
+                cmd = '''\
+                    ffmpeg \
+                    -protocol_whitelist file,http,https,tcp,tls,crypto \
+                    -i "{m3u8_file_url}" \
+                    -c copy {file_path}
+                    -user_agent "AppleCoreMedia/1.0.0.16A366 (iPhone; U; CPU OS 12_0 like Mac OS X; ja_jp)" \
+                    -headers "Accept: */*" \
+                    -headers "Accept-Language: ja-jp" \
+                    -headers "Accept-Encoding: gzip" \
+                    -headers "Connection: keep-alive" \
+                    -vn \
+                    '''.format(m3u8_file_url = episode.playlist_file_url, file_path = mp4_file_path).strip()
+                subprocess.call(cmd, shell=True)
+    
+                cmd = 'ffmpeg -y -i {mp4_file_path} -ab 192k {mp3_file_path}'.format(mp4_file_path = mp4_file_path, mp3_file_path = mp3_file_path)
+                subprocess.call(cmd, shell=True)
 
-            cmd = 'ffmpeg -y -i {mp4_file_path} -ab 192k {mp3_file_path}'.format(mp4_file_path = mp4_file_path, mp3_file_path = mp3_file_path)
-            subprocess.call(cmd, shell=True)
+                # embed id3 tag
+                Utils.embed_id3_tag(mp3_file_path, channel, episode)
+
+                logging.info("Download complete: " + channel.identifier_string)
+                twitter.notify_dl_completion(channel, episode)
+               
+        elif channel.broadcast_type == Channel.BroadcastType.RAW:
+            # download mp3 file directly
+            for episode in channel.episodes:
+                source_file_path = dir_path + episode.source_file_name()
+                if os.path.exists(source_file_path):
+                    logging.info("Skip " + channel.identifier_string + " " + episode.count_str + ", because already downloaded.")
+                    continue
+
+                try:
+                    response = urllib.request.urlopen(episode.source_file_url)
+                except urllib.error.HTTPError as e:
+                    if e.code == 404:
+                        message = "This episode may not be published."
+                        raise BusinessException(message)
+                    else:
+                        raise BusinessException(u"Unexpected error occured.")
+
+                out = open(source_file_path, "wb")
+                out.write(response.read())
+
+                # if source_file is mp4, convert to mp3
+                mp3_file_path = ""
+                if episode.is_mp4():
+                    mp3_file_path = dir_path + episode.source_file_name_without_extension() + ".mp3"
+                    
+                    cmd = 'ffmpeg -y -i {mp4_file_path} -ab 192k {mp3_file_path}'.format(mp4_file_path = source_file_path, mp3_file_path = mp3_file_path)
+                    subprocess.call(cmd, shell=True)
+
+                else:
+                    mp3_file_path = source_file_path
+
+                # embed id3 tag
+                Utils.embed_id3_tag(mp3_file_path, channel, episode)
+
+                logging.info("Download complete: " + channel.identifier_string)
+                twitter.notify_dl_completion(channel, episode)
 
         else:
-            # download mp3 file directly
-            try:
-                response = urllib.request.urlopen(channel.sound_url)
-            except urllib.error.HTTPError as e:
-                if e.code == 404:
-                    message = "This episode may not be published."
-                    raise BusinessException(message)
-                else:
-                    raise BusinessException(u"Unexpected error occured.")
-    
-            mp3_file_path = dir_path + channel.file_name
-            out = open(mp3_file_path, "wb")
-            out.write(response.read())
-
-        # embed id3 tag
-        Utils.embed_id3_tag(mp3_file_path, channel)
-
+            raise FatalException("")
 
     @staticmethod
-    def download_thumbnail(channel):
-        response = urllib.request.urlopen(channel.thumb_url)
+    def download_thumbnail(episode):
+        response = urllib.request.urlopen(episode.thumbnail_url)
         tmp_dir_path = Utils.tmp_dir_path()
-        thumb_file_path = tmp_dir_path + channel.thumb_file_name
+        thumb_file_path = tmp_dir_path + episode.thumb_file_name()
 
         if not os.path.exists(tmp_dir_path):
             os.makedirs(tmp_dir_path)
@@ -178,10 +244,10 @@ class Downloader:
 
         return thumb_file_path
 
-
 class Consts:
     BASE_URL = u"http://www.onsen.ag"
-    BASE_URL_GET_CHANNEL_INFO = u"http://www.onsen.ag/data/api/getMovieInfo/{channel_id}"
+    BASE_URL_GET_MOVIE_INFO = u"http://www.onsen.ag/data/api/getMovieInfo/{channel_id}"
+    BASE_URL_GET_PROGRAM_INFO = u"https://app.onsen.ag/api/me/programs/{program_id}"
     USER_SETTING_FILE_PATH = os.path.abspath(os.path.dirname(__file__)) + "/user_settings.yml"
     DEFAULT_ARTIST_NAME = u"onsen"
     DEFAULT_ALBUM_TITLE = u"{channel_title}"
@@ -207,7 +273,7 @@ class Utils:
         home = os.environ['HOME']
         script_dir = os.path.abspath(os.path.dirname(__file__))
         path = UserSettings.get("radio_save_path")\
-                           .replace("{channel_id}", channel.id)\
+                           .replace("{channel_id}", channel.identifier_string)\
                            .replace("{channel_title}", channel.title)\
                            .replace("~", home)\
                            .replace("./", script_dir + "/")
@@ -215,8 +281,8 @@ class Utils:
 
     # File path to save channel
     @staticmethod
-    def radio_save_file_path(channel):
-        return Utils.radio_save_dir_path(channel) + channel.file_name
+    def radio_save_file_path(channel, episode):
+        return Utils.radio_save_dir_path(channel) + episode.source_file_name()
 
     # Dir path to save temporary files
     @staticmethod
@@ -234,12 +300,12 @@ class Utils:
     # URL to get channel info
     @staticmethod
     def url_get_channel_info(channel_id):
-        return Consts.BASE_URL_GET_CHANNEL_INFO\
+        return Consts.BASE_URL_GET_MOVIE_INFO\
                      .replace("{channel_id}", channel_id)
 
     @staticmethod
-    def embed_id3_tag(file_path, channel):
-        cover_img_path = Downloader.download_thumbnail(channel)
+    def embed_id3_tag(file_path, channel, episode):
+        cover_img_path = Downloader.download_thumbnail(episode)
 
         tag = eyed3.load(file_path).tag
         tag.version = eyed3.id3.ID3_V2_4
@@ -249,15 +315,15 @@ class Utils:
         tag.album = Consts.DEFAULT_ALBUM_TITLE\
                           .format(channel_title=channel.title)
         tag.title = Consts.DEFAULT_TRACK_TITLE\
-                          .format(count=channel.count,
-                                  update=channel.updated_at)
+                          .format(count=episode.count_str,
+                                  update=episode.updated_date_str)
 
         tag.images.set(eyed3.id3.frames.ImageFrame.OTHER,
                        open(cover_img_path, "rb").read(),
                        "image/jpeg")
 
         try:
-            tag.track_num = int(channel.count)
+            tag.track_num = int(episode.count_str)
         except ValueError:
             tag.track_num = 0
 
@@ -271,6 +337,12 @@ class BusinessException(Exception):
     def __str__(self):
         return repr(self.value)
 
+class FatalException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 class Twitter:
     def __init__(self, consumer_key="", consumer_secret="", access_token_key="",
@@ -298,10 +370,10 @@ class Twitter:
     def set_in_reply_to(self, in_reply_to):
         self.in_reply_to = in_reply_to
 
-    def notify_dl_completion(self, channel):
+    def notify_dl_completion(self, channel, episode):
         message = u"録音が完了しました: 『{title} {count}話』 [{date}]"\
                   .format(title=channel.title,
-                          count=channel.count,
+                          count=episode.count_str,
                           date=datetime.now().strftime(u"%Y/%m/%d %H:%M"))
         self.post(message)
 
@@ -321,7 +393,8 @@ class Main:
                             filename='info.log',
                             level=logging.INFO)
 
-        # Setup notification
+        # Setup twitter notification
+        global twitter
         if UserSettings.get("twitter_settings") is not None:
             tw_settings = UserSettings.get("twitter_settings")
             twitter = Twitter(tw_settings["consumer_key"],
@@ -343,13 +416,7 @@ class Main:
                 c = Channel(c_id)
                 c.load_channel_info()
 
-                # Skip downloading if the downloaded-file already exists
-                file_path = Utils.radio_save_file_path(c)
-                if os.path.exists(file_path):
-                    logging.info("Skipped downloading, because already downloaded: " + c_id)
-                    continue
-                
-                Downloader.downloadChannel(c)
+                Downloader.download_channel(c)
 
             except BusinessException as e:
                 msg = "Not downloaded: " + c_id + ", because: " + e.value
@@ -359,9 +426,6 @@ class Main:
                 logging.error("Download interrupted: " + c_id)
                 logging.error(traceback.format_exc())
                 twitter.notify_dl_error(c_id)
-            else:
-                logging.info("Download complete: " + c_id)
-                twitter.notify_dl_completion(c)
 
         logging.info("Download finish.")
 
